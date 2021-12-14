d7b1cd2360e4fca5765faa3a94d8ef8756b454f2
tmpabdullah <tmp.abdullah1@outlook.com>
2021-12-15 01:52:17 +0500

    add other extensions support

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const nodePath = require('path');
+const path = require('path');
 const glob = require('glob');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports.htmlWebpackPluginGlob = function ({ path, ext = 'html' }) {
-  return glob
-    .sync(path + '/**/*.' + ext)
-    .map((src) => src.replace(path, ''))
-    .map(
-      (src) =>
-        new HtmlWebpackPlugin({
-          filename: src,
-          template: nodePath.resolve(path + src),
-        })
+module.exports.htmlWebpackPluginGlob = function ({
+  path: filePath,
+  ext = 'html',
+}) {
+  return glob.sync(filePath + '/**/*.' + ext).map((fileName) => {
+    const fileNameParsed = path.parse(fileName.replace(filePath, ''));
+
+    fileNameParsed.ext = '.html';
+    fileNameParsed.base = path.basename(
+      fileNameParsed.name + fileNameParsed.ext
     );
+
+    return new HtmlWebpackPlugin({
+      filename: path.format(fileNameParsed),
+      template: fileName,
+    });
+  });
 };
